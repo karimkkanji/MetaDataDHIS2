ce929a031d66b3df4ff631012cfb0b7d2424c6d8
Karim K. Kanji <karimkanji101@gmail.com>
2018-09-20 17:00:41 +0300

    20th Sept end of day

---
+++ src/ButtonGroup.js
@@ -0,0 +1,54 @@
+import React, {Component} from 'react';
+import * as ReactBootstrap from 'react-bootstrap';
+import './Tabpane.css';
+import LetterResults from "./LetterResults";
+import CustomAlert from "./CustomAlert";
+
+let valuePassed = "";
+
+class ButtonGroup extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            showComponent: false,
+        };
+        // This binding is necessary to make `this` work in the callback
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick(message) {
+        valuePassed = message;
+        this.setState({
+            showComponent: true,
+        });
+    }
+
+    genCharArray(charA, charZ) {
+        var a = [], i = charA.charCodeAt(0), j = charZ.charCodeAt(0);
+        for (i; i <= j; ++i) {
+            let letGot = String.fromCharCode(i);
+            a.push(<ReactBootstrap.Button onClick={this.handleClick.bind(this, letGot)}
+                                          key={i}>{String.fromCharCode(i)}</ReactBootstrap.Button>);
+        }
+        return a;
+    };
+
+    render() {
+        return (
+            <div className="container buttonGroup">
+                <ReactBootstrap.ButtonToolbar>
+                    <ReactBootstrap.ButtonGroup>
+                        <ReactBootstrap.Button onClick={this.handleClick.bind(this, "#")}>#</ReactBootstrap.Button>
+                        {this.genCharArray('A', 'Z')}
+                    </ReactBootstrap.ButtonGroup>
+                </ReactBootstrap.ButtonToolbar>
+                {this.state.showComponent ?
+                    <LetterResults letter={valuePassed} item={this.props.item}/> :
+                    <CustomAlert for={this.props.item}/>
+                }
+            </div>
+        );
+    }
+}
+
+export default ButtonGroup;
---
+++ src/Tabpane.js
@@ -0,0 +1,54 @@
+import React, {Component} from 'react';
+import './Tabpane.css';
+import {Nav, NavItem, Col, Row, Tab} from 'react-bootstrap';
+import ButtonGroup from "./ButtonGroup";
+import './Tabpane.css';
+
+class Tabpane extends Component {
+    constructor(props) {
+        super(props);
+        this.handleChangeState = this.handleChangeState.bind(this);
+    }
+
+    handleChangeState(message) {
+        this.InputChange(message);
+    }
+
+    InputChange = (params) => {
+        this.props.changeHandler(params);
+    };
+
+    render() {
+        return (
+            <div className="tabpanebody">
+                <div className="container">
+                    <Tab.Container id="tabs-with-dropdown" defaultActiveKey="first">
+                        <Row className="clearfix">
+                            <Col sm={12}>
+                                <Nav bsStyle="tabs">
+                                    <NavItem eventKey="first">Datasets</NavItem>
+                                    <NavItem eventKey="second">Indicators</NavItem>
+                                    <NavItem eventKey="third">Programs</NavItem>
+                                    <NavItem eventKey="fourth">Data Elements</NavItem>
+                                </Nav>
+                            </Col>
+                            <Col sm={12}>
+                                <div className={"container content"}>
+                                    <Tab.Content animation>
+                                        <Tab.Pane eventKey="first"><ButtonGroup item={"datasets"} stateHandler={this.props.dataSets}/></Tab.Pane>
+                                        <Tab.Pane eventKey="second"><ButtonGroup item={"indicators"}/></Tab.Pane>
+                                        <Tab.Pane eventKey="third"><ButtonGroup item={"programs"}/></Tab.Pane>
+                                        <Tab.Pane eventKey="fourth"><ButtonGroup item={"dataelements"}/></Tab.Pane>
+                                    </Tab.Content>
+                                </div>
+                            </Col>
+                        </Row>
+                    </Tab.Container>
+                </div>
+            </div>
+
+        );
+    }
+}
+
+export default Tabpane;
