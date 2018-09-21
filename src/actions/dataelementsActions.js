62596c5e20803e6d132aa05dfaf4022606da647a
Karim K. Kanji <karimkanji101@gmail.com>
2018-09-21 10:29:52 +0300

    Code order changed in directories

--- src/actions/dataelementsActions.js
+++ src/actions/dataelementsActions.js
@@ -1,21 +1,22 @@
+import config from "./config";
 const headers ={
     headers:{
-        'Authorization': `Basic ${btoa('evanpersie3@gmail.com:skolastikA97')}`
-}
+        'Authorization': `Basic ${btoa(config.username+":"+config.password)}`
+    }
 };
 
 export function fetchDataElements(){
     return function(dispatch){
-        fetch('http://197.136.81.99:8082/test/api/26/dataElements.json?paging=false&fields=:all', headers)
-        .then(res => res.json())
-        .then((response) => {
-            dispatch({ 
-                type: 'FETCH_DATAELEMENTS_FULLFILLED',
-                 payload: response});
-                 //console.log('dataelements fetched')
-                })
-        .catch((err) => {
-            dispatch({ type: 'FETCH_DATAELEMENTS_REJECTED', payload: err })
-        })
+        fetch(config.url+'/dataElements.json?paging=false&fields=:all', headers)
+            .then(res => res.json())
+            .then((response) => {
+                dispatch({
+                    type: 'FETCH_DATAELEMENTS_FULLFILLED',
+                    payload: response});
+                //console.log('dataelements fetched')
+            })
+            .catch((err) => {
+                dispatch({ type: 'FETCH_DATAELEMENTS_REJECTED', payload: err })
+            })
     }
 }
